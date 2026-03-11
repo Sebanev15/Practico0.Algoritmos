fda2f3d8d1da13a1c1a3d7728ba485bf64c9974a
Sebastian Neves <sebastian.neves@correo.ucu.edu.uy>
2026-03-11 09:04:20 -0300

    create ArithmeticDemo

---
+++ Practico0/src/main/java/org/example/ArithmeticDemo.java
@@ -0,0 +1,13 @@
+﻿package org.example;
+
+public class ArithmeticDemo {
+    public static void main(String[] args) {
+        int result = 1 + 2;
+        result = result - 1;
+        result = result * 2;
+        result = result / 2;
+        result = result + 8;
+        result = result % 7;
+    }
+
+}
