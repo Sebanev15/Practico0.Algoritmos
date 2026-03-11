367f5d67b2fe3f723e50013b037340fd3c1a0834
Joaquin de Dios <joaquin.dedios@correo.ucu.edu.uy>
2026-03-11 09:54:17 -0300

    ejercicio2

--- Practico0/src/main/java/org/example/ArithmeticDemo.java
+++ Practico0/src/main/java/org/example/ArithmeticDemo.java
@@ -2,12 +2,19 @@
 
 public class ArithmeticDemo {
     public static void main(String[] args) {
-        int result = 1 + 2;
-        result = result - 1;
-        result = result * 2;
-        result = result / 2;
-        result = result + 8;
-        result = result % 7;
+        int result = 1 + 2; // cambiar  a result =3
+        result = result - 1; //cambiar a result --
+        result = result * 2; // cambiar a result*=2
+        result = result / 2; //cambiar result/=2
+        result = result + 8; //cambiar a result +=8
+        result = result % 7; //cambiar a result %=7
+
+
+        int a =5;
+        int i =3;
+        a+= ++i;
+        System.out.println(a +" "+ i);
+
     }
 
 }
